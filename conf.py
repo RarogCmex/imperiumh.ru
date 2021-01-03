1c7cfb2feefcf02b0df0f2caea913afec348a12f
Denis Reva <denis7774@gmail.com>
2021-01-04 01:14:08 +0500

    Добавлены атрибуты

--- conf.py
+++ conf.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e7fcbf5152b145159412903d170bd533ed4ec5ca031be9d64dd473721a384e33
-size 55515
+oid sha256:d33b601f9ae66ca5d9dca678e848e486887efe70d2aabedee6bdf2dc9952be21
+size 55512
