f12d974c18682bda90e1d1ccf7f014100c89107e
Denis Reva <denis7774@gmail.com>
2021-08-10 22:10:58 +0500

    Мелкая заметка по gogs

--- conf.py
+++ conf.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2c6d09a5671697fc9e4a81968d8afa37c7819164fd2ef2cae92e747c361749da
-size 55737
+oid sha256:da72f8edd9bad43b577d82b35d67394b57168119898dfb153b4e1693903cd757
+size 55848
