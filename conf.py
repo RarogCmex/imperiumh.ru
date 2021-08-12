f9b9f843cef5e64bb48f99b08675a2ede852f9cb
Denis Reva <denis7774@gmail.com>
2021-08-12 12:21:49 +0500

    Добавлена система комментариев isso.

--- conf.py
+++ conf.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:da72f8edd9bad43b577d82b35d67394b57168119898dfb153b4e1693903cd757
-size 55848
+oid sha256:aaf7df23c8de236e162982aa1cce7a4628fb0a0c42a50884843a7e6c4078e852
+size 55885
